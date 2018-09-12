8221c44f0a569544492166686c76f77be5f4a4d1
Wilberto Morales <wilbertomorales777@gmail.com>
2018-09-12 00:11:55 -0500

    Remove stub spec

--- spec/aws_provisioner_spec.rb
+++ spec/aws_provisioner_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe AwsProvisioner do
   it "has a version number" do
     expect(AwsProvisioner::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
